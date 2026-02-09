774194d30ee091041fb78fc57c0c0032ea41140c
Richard Shann <richard@rshann.plus.com>
2026-02-09 16:00:32 +0000

    Do not allow starting on a rest when in PlayAlong mode

--- actions/menus/MainMenu/PlaybackMenu/DenemoPlayCursorToEnd.scm
+++ actions/menus/MainMenu/PlaybackMenu/DenemoPlayCursorToEnd.scm
@@ -1,4 +1,7 @@
-;DenemoPlayCursorToEnd
-(d-CreateTimebase)
-(d-SetPlaybackInterval (d-GetMidiOnTime) -1)
-(DenemoPlay)
+;;;;DenemoPlayCursorToEnd
+(if (and (d-PlayAlongActive) (Rest?))
+	(d-WarningDialog "Cannot start playalong from rest")
+	(begin
+		(d-CreateTimebase)
+		(d-SetPlaybackInterval (d-GetMidiOnTime) -1)
+		(DenemoPlay)))
