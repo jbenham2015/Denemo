04bb0f733521b54afde610139750f0e9d2071b77
Jeremiah Benham <jjbenham@chicagoguitar.com>
2026-06-21 15:18:15 +0000

    see if we can get some debugging info on this

--- src/core/utils.c
+++ src/core/utils.c
@@ -1666,6 +1666,12 @@ get_system_font_dir ()
       gchar *prefix = g_build_filename (get_prefix_dir (), NULL);
 #endif
       fontdir = g_build_filename (prefix, "share", "fonts", "truetype", "denemo", NULL);
+      g_warning("Attempting to add font dir: %s", fontpath);
+      struct stat st;
+      if (stat(fontpath, &st) != 0)
+      g_warning("Path does not exist: %s (%s)", fontpath, strerror(errno));
+      PangoFontMap *map = pango_cairo_font_map_get_default();
+      g_warning("Font map type: %s", G_OBJECT_TYPE_NAME(map));
     }
   return fontdir;
 }
