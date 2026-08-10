0e8b1e98870fbe46a534bf02d9a32acd00f36255
Richard Shann <richard@rshann.plus.com>
2026-08-10 15:29:49 +0100

    Do not try to set staff size on chord staffs, modernise script

--- actions/menus/ObjectMenu/StaffMenu/StaffPropertiesMenu/StaffSize.scm
+++ actions/menus/ObjectMenu/StaffMenu/StaffPropertiesMenu/StaffSize.scm
@@ -1,17 +1,19 @@
 ;;;StaffSize
 (let ((tag "StaffSize") (size #f))
-  (set! size (exact->inexact (/ (d-DirectiveGet-staff-minpixels tag) 10)))
-  (set! size (d-GetUserInput (_ "Setting Staff Size") (_ "Give Staff Size (for printing), 0 for default") (if size (number->string size) "0")))
-
-  (if (and size (string->number size))
-    (begin
-      (if (d-Directive-staff? tag)
-        (d-DirectiveDelete-staff tag))
-      (ToggleDirective "staff" "prefix" tag (string-append "
-     		   fontSize = #" size "
-     		   \\override VerticalAxisGroup.minimum-Y-extent = #'(0 . 0)
-     		   \\override StaffSymbol.staff-space = #(magstep " size ")\n ") (logior DENEMO_OVERRIDE_AFFIX DENEMO_ALT_OVERRIDE))
-      (set! size (inexact->exact (* (string->number size) 10)))
-           
-      (d-DirectivePut-staff-minpixels tag size))))
-      
+  (if (d-Directive-staff? "ChordStaff")
+	(d-WarningDialog "Cannot set staff size on Chord Staffs")
+	(begin
+		(set! size (d-DirectiveGet-staff-data tag))
+		(set! size (d-GetUserInput (_ "Setting Staff Size") (_ "Give Staff Size magnification (for printing), 0 for default") (if size size "0")))		
+		(if (equal? size "0")
+			(d-DirectiveDelete-staff tag)	
+			(if (and size (string->number size))
+				(begin
+				  (if (d-Directive-staff? tag)
+					(d-DirectiveDelete-staff tag))
+				  (ToggleDirective "staff" "prefix" tag (string-append "
+						   fontSize = #" size "
+						   \\override VerticalAxisGroup.minimum-Y-extent = #'(0 . 0)
+						   \\override StaffSymbol.staff-space = #(magstep " size ")\n ") (logior DENEMO_OVERRIDE_AFFIX DENEMO_ALT_OVERRIDE))
+				  (d-DirectivePut-staff-data tag size))
+				 (d-WarningDialog "Cancelled"))))))
